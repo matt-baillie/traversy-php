b443ed655e9b9f55b18f5755d84b31c68f61f75b
Matthew Baillie <mbaillie93@gmail.com>
2022-12-11 21:44:19 -0800

    Functions, array functions

--- 06_functions.php
+++ 06_functions.php
@@ -12,3 +12,43 @@
     // code to be executed
   }
 */
+function registeredUser($email)
+{
+  echo "User registered's email ${email}";
+}
+
+registeredUser('mbaillie@gmail.com');
+
+// Fucntion Scope
+// Scoped similarly to JS, with a fucntion scope and global scope
+// However, if a variable is scoped globally it cannot be accessed without declaring it with the global keyword within the function
+
+// $y = 12;
+
+// function logY()
+// {
+//   global $y;
+//   echo $y;
+// }
+
+// logY();
+// Return value & Default values
+function sum($n1 = 4, $n2 = 5)
+{
+  return $n1 + $n2;
+}
+$number = sum();
+echo $number;
+
+// Annonymous function
+// **Requires ; at the end of the unnamed function
+$subtract = function ($n1, $n2) {
+  return $n1 - $n2;
+};
+
+echo $subtract(6, 9);
+
+// arrow functions
+// fn instead of function, remove return and curly brackets
+$multiply = fn ($n1, $n2) =>  $n1 * $n2;
+echo $multiply(9, 9);
--- 07_array_functions.php
+++ 07_array_functions.php
@@ -5,3 +5,73 @@
   Functions to work with arrays
   https://www.php.net/manual/en/ref.array.php
 */
+$fruits = ['apple', 'orange', 'pear'];
+
+// length: count()
+// echo count($fruits);
+
+// Search Array: in_array()
+// var_dump(in_array('apple', $fruits));
+
+// Add to array: using array name and [] = value
+$fruits[] = 'grape';
+// or array_push(arg)
+array_push($fruits, 'banana', 'strawberry');
+// add to beginning
+array_unshift($fruits, 'blueberry');
+
+// Remove from array
+array_pop($fruits);
+array_shift($fruits);
+// remove a specific index. Actual index is removed; ie) indexes are now 0, 1, 3, 4,...
+// unset($fruits[2]);
+
+// Split into chunks of #, 2
+$chunked_array = array_chunk($fruits,  2);
+
+
+// print_r($chunked_array);
+// print_r($fruits);
+
+$arr1 = [1, 2, 3];
+$arr2 = [4, 5, 6];
+
+// Merging arrays & Spread operator
+$arr3 = array_merge($arr1, $arr2);
+$arr4 = [...$arr1, ...$arr2];
+
+// print_r($arr4);
+
+// print_r($arr3);
+
+// Combine keys and values in an Associative array
+$a = ['red', 'green', 'orange'];
+$b = ['avocado', 'apple', 'banana'];
+
+$c = array_combine($a, $b);
+// print_r($c);
+
+// $keys = array_keys($c);
+// print_r($keys);
+
+// Flip/swap key-values of array: array_flip()
+$flipped = array_flip($c);
+// print_r($flipped);
+
+// Create ranged array
+$numbers = range(1, 20);
+// print_r($numbers);
+
+// Map through array
+$new_numbers = array_map(function ($number) {
+  return "Number ${number}";
+}, $numbers);
+// print_r($new_numbers);
+
+// Filter through array
+$less_than_10 = array_filter($numbers, fn ($number) => $number < 10);
+// print_r($less_than_10);
+
+// Reduce: takes a carry over # and number
+$sum = array_reduce($numbers, fn ($carry, $number) => $carry + $number);
+var_dump($sum);
