105814d056a0d609231b5695289d5225b32e79d7
Matthew Baillie <mbaillie93@gmail.com>
2022-12-11 23:27:08 -0800

    Superglobals and get/post

--- 09_superglobals.php
+++ 09_superglobals.php
@@ -7,6 +7,7 @@
 /*
   $GLOBALS - A superglobal variable that holds information about any variables in global scope.
   $_GET - Contains information about variables passed through a URL or a form.
+
   $_POST -  Contains information about variables passed through a form.
   $_COOKIE - Contains information about variables passed through a cookie.
   $_SESSION - Contains information about variables passed through a session.
@@ -15,4 +16,28 @@
   $_FILES -  Contains information about files uploaded to the script.
   $_REQUEST - Contains information about variables passed through the form or URL.
 */
+// var_dump($_SERVER);
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h2>$_SERVER</h2>
+  <?php
+  foreach ($_SERVER as $key => $value) {
+    echo "<li>
+    ${key} - ${value} <br>
+    </li>";
+  }
+  ?>
+
+</body>
+
+</html>
--- 10_get_post.php
+++ 10_get_post.php
@@ -4,3 +4,24 @@
 /*
   We can pass data through urls and forms using the $_GET and $_POST superglobals.
 */
+if (isset($_POST['submit'])) {
+
+  echo $_POST['name'];
+  echo $_POST['age'];
+}
+?>
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Matt&age=30">Click</a>
+
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name">
+  </div>
+  <div>
+    <label for="age">Age:</label>
+    <input type="text" id="age" name="age">
+  </div>
+  <input type="submit" value="Submit" name="submit">
+
+
+</form>
