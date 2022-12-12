1e2b062079896c5e36e027a2323e42bc4db3c8fb
Matthew Baillie <mbaillie93@gmail.com>
2022-12-11 23:50:42 -0800

    Sanitizing inputs & cookie

--- 11_sanitizing_inputs.php
+++ 11_sanitizing_inputs.php
@@ -4,3 +4,35 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+if (isset($_POST['submit'])) {
+
+  // Multiple ways of sanitizing inputs**
+  // htmlspecialchars(variable)
+  // $name = htmlspecialchars($_POST['name']);
+  // $age = htmlspecialchars($_POST['age']);
+
+  // *** Preferred: filter_input(request-type, input-name, sanitation-rule)
+  // $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+  // $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+
+  // filter_var(variable, sanitation-rule)
+  $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+  $age = filter_var($_POST['age'], FILTER_SANITIZE_SPECIAL_CHARS);
+  echo $name . ' ' . $age;
+}
+?>
+
+
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name">
+  </div>
+  <div>
+    <label for="age">Age:</label>
+    <input type="text" id="age" name="age">
+  </div>
+  <input type="submit" value="Submit" name="submit">
+
+
+</form>
--- 12_cookies.php
+++ 12_cookies.php
@@ -4,3 +4,14 @@
 /*
   Cookies are a mechanism for storing data in the remote browser and thus tracking or identifying return users. You can set specific data to be stored in the browser, and then retrieve it when the user visits the site again.
 */
+
+// Setting cookie
+// setcookie(key, value, expiration-time)
+setcookie('name', "Matt", time() + 86400);
+
+if (isset($_COOKIE['name'])) {
+  echo $_COOKIE['name'];
+};
+
+// Delete cookie: set as nothing
+setcookie('name', '', time() - 86400);
