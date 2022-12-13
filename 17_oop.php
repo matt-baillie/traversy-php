50dc1a5c21bbf5425936c18f82158cf0a761d7fb
Matthew Baillie <mbaillie93@gmail.com>
2022-12-12 17:56:45 -0800

    file handling, uploading, exceptions and OOP'

--- 17_oop.php
+++ 17_oop.php
@@ -1,6 +1,18 @@
-<?php 
+<?php
 /* --- Object Oriented Programming -- */
 
 /*
   From PHP5 onwards you can write PHP in either a procedural or object oriented way. OOP consists of classes that can hold "properties" and "methods". Objects can be created from classes.
 */
+class User
+{
+  // Properties are attributes that belong to a class
+  public $name;
+  public $email;
+  public $password;
+}
+
+// Instantiate a user object
+$user1 = new User();
+$user1->name = "Matt";
+var_dump($user1);
